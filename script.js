766be59e5cc2a12a08e38304551125d7e5286afa
ptriv1 <84185336+ptriv1@users.noreply.github.com>
2021-07-10 19:34:28 -0400

    created character and empty arrays

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ var generateBtn = document.querySelector("#generate");
 
 window.prompt("Please enter a password length:");
 
+
+!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+var specialCharacters = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var result;
+var possibleCharacters = [];
+var guaranteedCharacters = [];
+
+
+function generatePassword {
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
