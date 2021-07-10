bf95e06ccca671677004be3fdc1d00d4f8df805e
ptriv1 <84185336+ptriv1@users.noreply.github.com>
2021-07-10 19:35:52 -0400

    wrote comments

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Prompt user
 window.prompt("Please enter a password length:");
 
-
-!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+// Arrays
 var specialCharacters = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -13,7 +13,7 @@ var result;
 var possibleCharacters = [];
 var guaranteedCharacters = [];
 
-
+// Generate password function
 function generatePassword {
 
 }
