0cafe7d101dfeb46d25c3974e5ec881c8852e4a1
ptriv1 <84185336+ptriv1@users.noreply.github.com>
2021-07-16 15:17:56 -0400

    comments to explain what is in code

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Assignment Code
+
+// Global variables
 var generateBtn = document.querySelector("#generate");
 
 var special;
@@ -19,16 +21,16 @@ var userInput;
 
 
 
-// Prompt user
 
 
-// Arrays
+
+// Arrays for types of characters
 var specialCharacters = ["!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerCasedCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCasedCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-
+// Generate Password function
 function generatePassword() {
   
     // Ask user for their choice
@@ -55,10 +57,12 @@ function generatePassword() {
   
       }
 
+  // Arrays for result, possible characters, and guaranteed characters
   var resultCharacters = [];
   var possibleCharacters = [];
   var guaranteedCharacters = [];
 
+  // Checking for special, numeric, upper case, and/or lower case
   if (special || numeric || upperCase || lowerCase) {
     if (special) {
       possibleCharacters = possibleCharacters.concat(specialCharacters);
@@ -90,6 +94,7 @@ function generatePassword() {
       guaranteedCharacters.push(randomLowerCase);
     }
 
+    // Looping through user input of requested characters
     for (i = 0; i < userInput; i++) {
       resultIndex = Math.floor(Math.random() * possibleCharacters.length);
       resultCharacters.push(possibleCharacters[resultIndex]);
@@ -99,7 +104,7 @@ function generatePassword() {
       resultCharacters[i] = guaranteedCharacters[i];
     }
     
-    
+    // Return password without commas or spaces
     return resultCharacters.join("");
   }
 
